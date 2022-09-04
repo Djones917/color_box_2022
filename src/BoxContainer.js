e6569b4b7a3e4ea823c0ea5960a1b8f760cbc184
Derek <nightwing917@hotmail.com>
2022-09-03 20:09:52 -0800

    added cont boxes map

--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,7 +1,15 @@
+import { render } from '@testing-library/react';
 import React, { Component } from 'react';
 
 class BoxContainer extends Component {
-
+   static defaultProps = {
+      numBoxes: 18
+   }
+   render() {
+      const boxes = Array.from({length: this.props.numBoxes}).map(
+        () => <Box />
+      ) 
+   }
 }
 
 export default BoxContainer;
